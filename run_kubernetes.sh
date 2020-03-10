36147fa7951dda81f9e6e348c4e7f34647263006
dojutsu-user <vaibhgupt199@gmail.com>
2020-03-11 00:17:06 +0530

    kubernetes output

---
+++ run_kubernetes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="vaib79/prediction"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run prediction\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=prediction
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward prediction 8000:80
